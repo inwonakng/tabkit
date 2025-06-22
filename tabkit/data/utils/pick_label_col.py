4735e0e1a83f314df230c26e57f42e97abea43d3
inwonakng <inwon.kang04@gmail.com>
2025-06-22 16:29:16 -0400

    better label picking mechanism

--- tabkit/data/utils/pick_label_col.py
+++ tabkit/data/utils/pick_label_col.py
@@ -1,10 +1,15 @@
 from typing import Literal
 import pandas as pd
+ 
+from .column_metadata import ColumnMetadata
 
 
 def pick_label_col(
     df: pd.DataFrame,
     random_state: int | None = None,
+    allowed_kind: list[str] | None = None,
+    allowed_dtype: list[str] | None = None,
+    exclude_columns: list[str] | None = None,
 ) -> str:
     """
     Given a dataframe, pick the column that can be used as the prediction target.
@@ -16,6 +21,21 @@ def pick_label_col(
     scores = pd.Series(index=df.columns, dtype=float)
 
     for col in df.columns:
+        if exclude_columns is not None and col in exclude_columns:
+            # skip excluded columns
+            scores.pop(col)
+            continue
+
+        col_info = ColumnMetadata.from_series(df[col])
+        if allowed_kind is not None and col_info.kind not in allowed_kind:
+            # skip columns that are not of the allowed kind
+            scores.pop(col)
+            continue
+        if allowed_dtype is not None and col_info.dtype not in allowed_dtype:
+            # skip columns that are not of the allowed dtype
+            scores.pop(col)
+            continue
+
         n_unique = df[col].nunique()
         n_missing = df[col].isna().sum()
         # now we will assign a score based on the data type
