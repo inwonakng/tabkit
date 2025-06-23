dcc9366c1553e65808b98674aee4f210a286b1cd
inwonakng <inwon.kang04@gmail.com>
2025-06-22 21:31:41 -0400

    remove torch dependency

--- tabkit/utils/__init__.py
+++ tabkit/utils/__init__.py
@@ -1,14 +1,12 @@
 from .configuration import Configuration
 from .logger import setup_logger, silence_logger
 from .random_id import get_random_id
-from .safe_json import safe_json
 from .slugify import slugify
 
 __all__ = [
     "setup_logger",
     "silence_logger",
     "Configuration",
-    "safe_json",
     "get_random_id",
     "slugify",
 ]
